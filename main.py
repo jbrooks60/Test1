f9a46c77d942efcba8647c330c838f03d2882aa1
jbrooks6 <jbrooks6@student.strode-college.ac.uk>
2020-11-06 11:16:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# My first program
+# Subroutine to output text
+
+def OutputText():
+  print("Anyone can write code that computers understand.Good programmers write code humans can understand")
+
+# Main program
+OutputText()
